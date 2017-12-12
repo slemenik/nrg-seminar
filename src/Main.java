86bd0874d5672c97199f9a079bba9cdb1ec82197
slemenik <ms4388@student.uni-lj.si>
2017-12-12 16:27:42 +0100

    Downloading .las file from web

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,25 @@ import com.github.mreutegg.laszip4j.LASPoint;
 import com.github.mreutegg.laszip4j.LASReader;
 import com.github.mreutegg.laszip4j.laszip.LASpoint;
 import org.apache.commons.io.FileUtils;
+import org.glassfish.jersey.client.internal.HttpUrlConnector;
 
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+
 
 
 public class Main {
@@ -23,23 +34,31 @@ public class Main {
         //write to file
         //http://gis.arso.gov.si/lidar/gkot/b_23/D48GK/GK_545_141.zlas
 
+        Client client = ClientBuilder.newClient();
+        WebTarget resource = client.target(URI.create("http://gis.arso.gov.si/lidar/gkot/laz/b_35/D48GK/GK_470_97.laz"));
+        Invocation.Builder request = resource.request();
+        request.accept(MediaType.APPLICATION_OCTET_STREAM);
 
-//        URL url = null;
-//        try {
-//            url = new URL("http://gis.arso.gov.si/lidar/gkot/b_23/D48GK/GK_545_141.zlas");
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        }
-//        File f = new File("temp");
-//        //System.out.print(f.getName());
-//
-//        try {
-//            FileUtils.copyURLToFile(url, f);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        Response response = request.get();
 
-        File f = new File("C:\\Users\\Matej\\IdeaProjects\\nrg-seminar\\GK_521_144.laz");
+
+        System.out.println(response.getEntity().getClass());
+
+        InputStream inputStream = response.readEntity(InputStream.class);
+
+        File f = new File("temp.laz");
+        f.deleteOnExit();
+
+
+        try {
+            FileUtils.copyInputStreamToFile(inputStream, f);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //inputStream.close();
+
+        //File f = new File("C:\\Users\\Matej\\IdeaProjects\\nrg-seminar\\GK_521_144.laz");
         LASReader lasReader = null;
         try {
             lasReader = new LASReader(f);
@@ -48,13 +67,12 @@ public class Main {
         }
 
         int i = 0;
-        for (LASPoint p:
-             lasReader.getPoints()) {
+        for (LASPoint p: lasReader.getPoints()) {
             byte b = p.getClassification();
+
             if (i++>20) break;
-            System.out.println(b);
+            System.out.println(p.getX());
         }
-        System.out.print(1);
 
 
     }

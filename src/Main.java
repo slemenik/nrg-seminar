347ccf7624ba4fc3a677098cb62a96c26da03dd4
slemenik <ms4388@student.uni-lj.si>
2017-12-12 23:20:26 +0100

    Reading OrtoPhoto from web to BufferedImage

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ import org.apache.commons.io.FilenameUtils;
 import org.glassfish.jersey.client.internal.HttpUrlConnector;
 
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.Form;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,42 +27,47 @@ import java.nio.file.Paths;
 public class Main {
 
 
-    public static final String ARSO_LIDAR_URL = "http://gis.arso.gov.si/lidar/gkot/laz/b_35/D48GK/GK_470_97.laz";
+    private static final String ARSO_LIDAR_URL = "http://gis.arso.gov.si/lidar/gkot/laz/b_35/D48GK/GK_470_97.laz";
+    private static final String ARSO_ORTOPHOTO_URL = "http://gis.arso.gov.si/arcgis/rest/services/DOF_2016/MapServer/export";
 
     public static void main(String[] args){
 
         System.out.println("Started...");
 
         //download LIDAR file
-        File file = createLidarFile(ARSO_LIDAR_URL);
-
-
+//        File file = createLidarFile(ARSO_LIDAR_URL);
+        File file = new File("C:\\Users\\Matej\\IdeaProjects\\nrg-seminar\\GK_470_97.laz");
         //calculate colors
+
+        String[] fileNameParams = FilenameUtils.removeExtension(file.getName()).split("_"); //GK_470_97
+        BufferedImage image = getOrtoPhoto(Integer.parseInt(fileNameParams[1]),Integer.parseInt(fileNameParams[2]));//470,97
+
+//        LASReader lasReader = null;
+//        try {
+//            lasReader = new LASReader(file);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        int i = 0;
+//        System.out.println("Reading points...");
+//        for (LASPoint p: lasReader.getPoints()) {
+//            byte b = p.getClassification();
+//
+//            if (i++>20) break;
+//            System.out.println(p.getX());
+//        }
+
+
         //calculate normals
         //write to file
-
-        LASReader lasReader = null;
-        try {
-            lasReader = new LASReader(file);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        int i = 0;
-        System.out.println("Reading points");
-        for (LASPoint p: lasReader.getPoints()) {
-            byte b = p.getClassification();
-
-            if (i++>20) break;
-            System.out.println(p.getX());
-        }
-
+        System.exit(0);
 
     }
 
     public static File createLidarFile(String URL){
         File f = null;
-        InputStream inputStream = null;
+        InputStream inputStream;
         try {
             Client client = ClientBuilder.newClient();
             WebTarget resource = client.target(URI.create(ARSO_LIDAR_URL));
@@ -74,8 +79,9 @@ public class Main {
 
             System.out.print("Reading response...");
             inputStream = response.readEntity(InputStream.class);
+            response.close();
 
-            String fileName = FilenameUtils.getName(resource.getUri().getPath());
+            String fileName = FilenameUtils.getBaseName(resource.getUri().getPath());
             f = new File(fileName);
             f.deleteOnExit();
 
@@ -90,4 +96,31 @@ public class Main {
 
         return f;
     }
+
+    private static BufferedImage getOrtoPhoto(int leftX, int lowerY){
+
+        double minX = leftX * 1000;
+        double minY = lowerY * 1000;
+        double maxX = minX + (999.999999999);
+        double maxY = minY + (999.999999999);
+
+        Client client = ClientBuilder.newClient();
+        Form form = new Form()
+                .param("bbox", minX + ","+minY+","+maxX+","+maxY)
+                .param("format", "bmp")
+                .param("transparent", "false")
+                .param("f", "image")
+                .param("size", "1000,1000");
+
+        System.out.print("Requesting from source: [" + ARSO_ORTOPHOTO_URL + "]");
+        Response response = client.target(ARSO_ORTOPHOTO_URL).request().post(Entity.form(form));
+        BufferedImage image = response.readEntity(BufferedImage.class);
+        response.close();
+        System.out.println("[DONE]");
+
+
+        return image;
+    }
+
+
 }
